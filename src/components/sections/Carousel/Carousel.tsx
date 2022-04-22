e9ec1a0d4060c26517ddb2a87c2f1b053c05d61c
Carmine Porricelli <carmineporricelli96@gmail.com>
2022-04-22 11:21:25 +0200

    Added Divider Video/Carousel

--- src/components/sections/Carousel/Carousel.tsx
+++ src/components/sections/Carousel/Carousel.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import s from "./Carousel.module.css";
 import config from "../../../config/carousel.json";
 import { useTranslation } from "react-i18next";
+import Container from "../../layout/Container";
 // import Section from "../../layout/Section";
 
 interface CarouselProps {
@@ -14,6 +15,11 @@ const Carousel: React.FC<CarouselProps> = () => {
 
   return (
     <section>
+      <div className="bg-primary py-4">
+        <Container>
+          <h2 className="text-white text-3xl font-bold border-l-4 border-white pl-4">Carousel</h2>
+        </Container>
+      </div>
       <Swiper
         className={s.root}
         modules={[Navigation, Pagination, A11y, Autoplay]}
